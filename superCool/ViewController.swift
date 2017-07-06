f1cf395fdba90729483154d7b26b908ac093d9f5
apple <apple@apples-MacBook-Pro.local>
2017-07-06 21:25:45 +0430

    first commit for SuperCool

--- superCool/ViewController.swift
+++ superCool/ViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolTitle: UIImageView!
+    @IBOutlet weak var coolBackGround: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func MakeSuperColl(){
+        coolTitle.isHidden = false
+        coolBackGround.isHidden = false
+        
     }
-
 
 }
 
